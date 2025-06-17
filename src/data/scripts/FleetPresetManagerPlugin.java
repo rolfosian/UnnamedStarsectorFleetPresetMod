9a922894c097a3419a082b60ab316f9c11b0096b
rolfosian <135147406+rolfosian@users.noreply.github.com>
2025-06-17 12:11:25 +1200

    disable console

--- src/data/scripts/FleetPresetManagerPlugin.java
+++ src/data/scripts/FleetPresetManagerPlugin.java
@@ -39,12 +39,12 @@ public class FleetPresetManagerPlugin extends BaseModPlugin {
     }
     private static final String ver = "0.0.9d";
 
-    @Override
-    public void onApplicationLoad() {
-        CustomConsoleAppender consoleAppender = new CustomConsoleAppender();
-        consoleAppender.setLayout(new PatternLayout("%d{HH:mm:ss} %-5p - %m%n"));
-        Logger.getRootLogger().addAppender(consoleAppender);
-    }
+    // @Override
+    // public void onApplicationLoad() {
+    //     CustomConsoleAppender consoleAppender = new CustomConsoleAppender();
+    //     consoleAppender.setLayout(new PatternLayout("%d{HH:mm:ss} %-5p - %m%n"));
+    //     Logger.getRootLogger().addAppender(consoleAppender);
+    // }
     
     @Override
     public void onGameLoad(boolean newGame) {
