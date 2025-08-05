bd99ec5719904b486b82d155549ba59bc58b473c
rolfosian <135147406+rolfosian@users.noreply.github.com>
2025-08-05 23:02:14 +1200

    Update TreeTraverser.java

--- src/data/scripts/ui/TreeTraverser.java
+++ src/data/scripts/ui/TreeTraverser.java
@@ -4,6 +4,8 @@ import java.util.*;
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.ui.ButtonAPI;
 import com.fs.starfarer.api.ui.LabelAPI;
+import com.fs.starfarer.api.ui.UIComponentAPI;
+import com.fs.starfarer.api.ui.UIPanelAPI;
 
 import data.scripts.ClassRefs;
 import data.scripts.util.PresetMiscUtils;
@@ -13,21 +15,21 @@ import data.scripts.util.ReflectionUtilis;
 public class TreeTraverser {
 
     public static class TreeNode {
-        private Object parent;
-        private List<Object> children;
+        private UIPanelAPI parent;
+        private List<UIComponentAPI> children;
         private int depth;
 
-        public TreeNode(Object parent, List<Object> children, int depth) {
+        public TreeNode(UIPanelAPI parent, List<UIComponentAPI> children, int depth) {
             this.parent = parent;
             this.children = children;
             this.depth = depth;
         }
 
-        public Object getParent() {
+        public UIPanelAPI getParent() {
             return this.parent;
         }
         
-        public List<Object> getChildren() {
+        public List<UIComponentAPI> getChildren() {
             return this.children;
         }
 
@@ -38,7 +40,7 @@ public class TreeTraverser {
         public List<ButtonAPI> getButtons() {
             List<ButtonAPI> buttons = new ArrayList<>();
 
-            for (Object child : this.children) {
+            for (UIComponentAPI child : this.children) {
                 if (ButtonAPI.class.isAssignableFrom(child.getClass())) buttons.add((ButtonAPI)child);
             }
             return buttons;
@@ -47,7 +49,7 @@ public class TreeTraverser {
         public List<LabelAPI> getLabels() {
             List<LabelAPI> labels = new ArrayList<>();
 
-            for (Object child : this.children) {
+            for (UIComponentAPI child : this.children) {
                 if (LabelAPI.class.isAssignableFrom(child.getClass())) labels.add((LabelAPI)child);
             }
             return labels;
@@ -57,7 +59,7 @@ public class TreeTraverser {
     private final Object parentPanel;
     private List<TreeNode> nodes;
     private TreeNode targetNode = null;
-    private Object targetChild = null;
+    private UIComponentAPI targetChild = null;
     private int currentIndex;
     
     // get entire panel tree
@@ -81,58 +83,58 @@ public class TreeTraverser {
     public void refresh() {
         this.nodes = new ArrayList<>();
         this.currentIndex = 0;
-        this.getChildren(parentPanel, 0);
+        this.getChildren((UIComponentAPI)parentPanel, 0);
     }
 
     public void refresh(int depthLimit) {
         this.nodes = new ArrayList<>();
         this.currentIndex = 0;
-        this.getChildren(parentPanel, 0, depthLimit);
+        this.getChildren((UIComponentAPI)parentPanel, 0, depthLimit);
     }
 
     public void refresh(int... treePath) {
         this.nodes = new ArrayList<>();
         this.currentIndex = 0;
-        this.getChildren(parentPanel, 0, treePath);
+        this.getChildren((UIComponentAPI)parentPanel, 0, treePath);
         
         this.targetNode = this.nodes.get(nodes.size()-1);
         this.targetChild = this.targetNode.getChildren().get(treePath[treePath.length-1]);
     }
 
-    private void getChildren(Object parent, int depth) {
-        List<Object> children = ClassRefs.uiPanelClass.isInstance(parent) ? (List<Object>) ReflectionUtilis.invokeMethodDirectly(ClassRefs.uiPanelgetChildrenCopyMethod, parent) : null;
+    private void getChildren(UIComponentAPI parent, int depth) {
+        List<UIComponentAPI> children = ClassRefs.uiPanelClass.isInstance(parent) ? (List<UIComponentAPI>) ReflectionUtilis.invokeMethodDirectly(ClassRefs.uiPanelgetChildrenCopyMethod, parent) : null;
 
         if (children != null && !children.isEmpty()) {
-            this.nodes.add(new TreeNode(parent, children, depth));
+            this.nodes.add(new TreeNode((UIPanelAPI)parent, children, depth));
             depth++;
 
-            for (Object child : children) {
+            for (UIComponentAPI child : children) {
                 this.getChildren(child, depth);
             }
         }
         return;
     }
 
-    private void getChildren(Object parent, int depth, int depthLimit) {
-        List<Object> children = ClassRefs.uiPanelClass.isInstance(parent) ? (List<Object>) ReflectionUtilis.invokeMethodDirectly(ClassRefs.uiPanelgetChildrenCopyMethod, parent) : null;
+    private void getChildren(UIComponentAPI parent, int depth, int depthLimit) {
+        List<UIComponentAPI> children = ClassRefs.uiPanelClass.isInstance(parent) ? (List<UIComponentAPI>) ReflectionUtilis.invokeMethodDirectly(ClassRefs.uiPanelgetChildrenCopyMethod, parent) : null;
 
         if (children != null && !children.isEmpty()) {
-            this.nodes.add(new TreeNode(parent, children, depth));
+            this.nodes.add(new TreeNode((UIPanelAPI)parent, children, depth));
             depth++;
             if (depth == depthLimit) return;
 
-            for (Object child : children) {
+            for (UIComponentAPI child : children) {
                 this.getChildren(child, depth, depthLimit);
             }
         }
         return;
     }
 
-    private void getChildren(Object parent, int depth, int... treePath) {
-        List<Object> children = ClassRefs.uiPanelClass.isInstance(parent) ? (List<Object>) ReflectionUtilis.invokeMethodDirectly(ClassRefs.uiPanelgetChildrenCopyMethod, parent) : null;
+    private void getChildren(UIComponentAPI parent, int depth, int... treePath) {
+        List<UIComponentAPI> children = ClassRefs.uiPanelClass.isInstance(parent) ? (List<UIComponentAPI>) ReflectionUtilis.invokeMethodDirectly(ClassRefs.uiPanelgetChildrenCopyMethod, parent) : null;
 
         if (children != null && !children.isEmpty()) {
-            this.nodes.add(new TreeNode(parent, children, depth));
+            this.nodes.add(new TreeNode((UIPanelAPI)parent, children, depth));
             depth++;
             if (depth == treePath.length) return;
 
@@ -141,9 +143,9 @@ public class TreeTraverser {
         return;
     }
 
-    public List<Integer> getPathToTarget(Object target) {
+    public List<Integer> getPathToTarget(UIComponentAPI target) {
         List<Integer> result = new ArrayList<>();
-        Object current = target;
+        UIComponentAPI current = target;
 
         while (!(current == this.parentPanel)) {
             outer:
@@ -163,23 +165,22 @@ public class TreeTraverser {
         return result;
     }
 
-    public void logPathToTarget(Object target) {
+    public void logPathToTarget(UIComponentAPI target) {
         Global.getLogger(getClass()).info("Path to target: " + String.valueOf(getPathToTarget(target)));
     }
 
     public void removeBranch() {
         TreeNode node = getCurrentNode();
-        Object parent = node.getParent();
+        UIPanelAPI parent = node.getParent();
         if (parent == null) return;
 
-        UIPanel pp =  new UIPanel(parent);
-        for (Object child : node.getChildren()) {
-            pp.remove(new UIComponent(child));
+        for (UIComponentAPI child : node.getChildren()) {
+            parent.removeComponent(child);
         }
         refresh();
     }
 
-    public Object getTargetChild() {
+    public UIComponentAPI getTargetChild() {
         return this.targetChild;
     }
 
@@ -225,12 +226,13 @@ public class TreeTraverser {
         currentIndex = 0;
     }
 
-    public void clearPanel() { // this is dangerous to use and i cant be bothered debugging it, sometimes it goes above the top level for some reason idk. it's good for clearing ConfirmDialog panels though
+    public void clearPanel() {
         while (this.goDownOneLevel()) {}
         for (int i = 0; i < this.getNodes().size(); i++) {
-            UIPanel par = new UIPanel(this.getCurrentNode().getParent());
-            for (Object child : this.getCurrentNode().getChildren()) {
-                par.remove(new UIComponent(child));
+            UIPanelAPI parent = this.getCurrentNode().getParent();
+
+            for (UIComponentAPI child : this.getCurrentNode().getChildren()) {
+                parent.removeComponent(child);
             }
             this.goUpOneLevel();
         }
