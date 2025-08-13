94afcd2bb412ac43ce129e04fb872827420a80be
rolfosian <135147406+rolfosian@users.noreply.github.com>
2025-08-13 14:33:53 +1200

    Update FleetPresetManagementListener.java

--- src/data/scripts/listeners/FleetPresetManagementListener.java
+++ src/data/scripts/listeners/FleetPresetManagementListener.java
@@ -771,7 +771,7 @@ public class FleetPresetManagementListener extends ActionListener {
             }
         }
 
-        private void handleArrowKeys(int key, int rowNum) {
+        private void navigateTableRows(int key, int rowNum) {
             int newIndex = key == Keyboard.KEY_UP ? (tableUp ? selectedRowIndex - 1 : selectedRowIndex + 1) : (tableUp ? selectedRowIndex + 1 : selectedRowIndex - 1);
             newIndex = newIndex < 0 ? rowNum - 1 : newIndex == rowNum ? 0 : newIndex;
             if (newIndex == selectedRowIndex && rowNum == 1) return;
@@ -799,7 +799,7 @@ public class FleetPresetManagementListener extends ActionListener {
                         boolean keyUp = Keyboard.isKeyDown(Keyboard.KEY_UP);
 
                         if (keyDown || keyUp) {
-                            handleArrowKeys(event.getEventValue(), rowNum);
+                            navigateTableRows(event.getEventValue(), rowNum);
 
                         } else if (isKeyDownEvent) {
                             if (Keyboard.isKeyDown(Keyboard.KEY_ESCAPE) && rowNum > 0 && selectedPresetName != EMPTY_STRING) {
